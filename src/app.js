b65d0377d673836d49144082823b182853fac3db
csakarya1 <csakarya12@gmail.com>
2024-08-12 17:33:10 +0300

    12 haziran denemesi

--- src/app.js
+++ src/app.js
@@ -1,36 +1,33 @@
-console.log("Merhaba Kodlama.io")
+console.log("Merhaba Kodlama.io");
 
 //JS type safe değildir
-let dolarBugun = 9.30
+let dolarDun = 9.2;
+let dolarBugun = 9.3;
 
-let dolarDun = 9.20
-dolarDun = "9.20"
 {
-    let dolarDun = 9.10
+  let dolarDun = 9.1;
 }
+console.log(dolarDun);
 
-console.log(dolarDun)
+const euroDun = 11.2;
+// euroDun = 11; // bunları yapamıyoruz
 
-const euroDun = 11.2
-//euroDun = 11
-
-console.log(euroDun)
-
+console.log(euroDun);
 //array
 //camelCasing
 //PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+let konutKredileri = [
+  "Konut Kredisi",
+  "Emlak Konut Kredisi",
+  "Kamu Konut Kredisi",
+  "özel konut kredileri",
+];
 
-console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
-    console.log("<li>"+konutKredileri[i]+"</li>")
+console.log("<ul>");
+for (let i = 0; i < konutKredileri.length; i++) {
+  console.log("<li>" + konutKredileri[i] + "</li>");
 }
-console.log("</ul>")
 
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
+console.log("</ul>");
 
-console.log(konutKredileri)
+console.log(konutKredileri);
